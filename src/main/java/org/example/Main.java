46aa0b78949c7a757e77666402bd47782f386f9d
karina bulanova <karinasirokova5442@gmail.com>
2023-04-21 11:57:18 +0300

    change src

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Post post = new Post();
+        post.name = "Иван";
+        post.passport = "3333№444444";
+        post.patronymic = "Иванович";
+        post.phone = "+78889990077";
+        post.surname = "Иванов";
+        post.subscription = true;
+
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
+
     }
 }
